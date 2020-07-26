a8a41969a5f1990e2828c733ddc2d2d126af7a2b
Enrique Berrueta <eabz@polispay.org>
2020-07-26 13:34:18 -0500

    Fix script

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-## Source: https://gist.github.com/brianclements/f72b2de8e307c7b56689
 ## This script will remove all docker images and containers
 
-docker rm -vf $(docker ps -a -q) 2>/dev/null || echo "No more containers to remove."
-docker rmi $(docker images -q) 2>/dev/null || echo "No more images to remove."
+docker system prune -f 
 
 exit 0
